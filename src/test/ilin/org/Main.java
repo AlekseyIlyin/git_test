341d1dc43ed27f4d6888a08d74996e5e8378c676
Aleksey Ilin <kayrus@mail.ru>
2022-02-07 17:19:06 +0300

    add second line

--- src/test/ilin/org/Main.java
+++ src/test/ilin/org/Main.java
@@ -3,5 +3,6 @@ package test.ilin.org;
 public class Main {
     public static void main(String[] args) {
         System.out.println("first line");
+        System.out.println("second line");
     }
 }
